12d12847716c90c9f9eeb9eced4e33c620026dfb
TheFluffyNeubla <jasonyin30@gmail.com>
2024-08-06 21:16:45 -0400

    init socket.io imports

--- server/src/sockets/roomSockets.js
+++ server/src/sockets/roomSockets.js
@@ -4,14 +4,14 @@ export default (io) => {
   io.on("connection", (socket) => {
     console.log("a user connected");
 
-    socket.on("join", (roomId, userId) => {
-      socket.join(roomId);
-      const result = roomUtils.joinRoom(roomId, userId);
-      io.emit("didJoin", result);
-    });
+    // socket.on("join", (roomId, userId) => {
+    //   socket.join(roomId);
+    //   const result = roomUtils.joinRoom(roomId, userId);
+    //   io.emit("didJoin", result);
+    // });
 
-    socket.onAny(() => {
-      console.log("something happened");
-    });
+    // socket.onAny(() => {
+    //   console.log("something happened");
+    // });
   });
 };
