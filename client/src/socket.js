1606eae2eacb030e34b6f6a655dcea7674c8b24d
Jason Yin <jyin23@ncsu.edu>
2025-02-23 00:50:47 -0500

    export error

--- client/src/socket.js
+++ client/src/socket.js
@@ -9,8 +9,6 @@ const URL = "http://localhost:3001";
 // transports: ['websocket'] // disable xhr poll
 // export const socket = io(URL);
 const SERVER_URL = process.env.REACT_APP_SERVER_URL || "https://hearts-q4nx.onrender.com";
-const socket = io(SERVER_URL, {
+export const socket = io(SERVER_URL, {
     transports: ["websocket", "polling"],
 });
-
-export default socket;
